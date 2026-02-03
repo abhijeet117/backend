8c25fcc54ec3686ee57c74701f3f086837b8465d
abhijeet117 <abhijeetkumarsah92@gmail.com>
2026-02-03 22:43:16 +0530

    Working on day-10

---
+++ Day-10/Frontend/src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+function App() {
+
+  const [note, setNote] = useState([
+    
+  ]);
+
+  function getAllNotes() {
+      axios.get("http://localhost:3000/api/notes").then((res) => {
+      setNote(res.data.note);
+      console.log(res.data);
+    })
+    }
+
+  useEffect(() => {
+
+    getAllNotes();
+  
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+
+    const {tittle, description} = e.target.elements
+
+    console.log(tittle.value , description.value)
+
+    axios.post("http://localhost:3000/api/notes",{
+      tittle : tittle.value,
+      description: description.value
+    })
+    .then(res =>{
+      console.log(res.data);
+      getAllNotes();
+    })
+
+    
+  }
+  
+
+  return (
+    <>
+
+    <form onSubmit={handleSubmit}>
+      <input name="tittle" type="text" placeholder="Enter Tittle" />
+      <input name="description" type="text" placeholder="Enter Description" />
+      <button >Create Note</button>
+    </form>
+
+
+
+      {note.map((note, idx) => {
+        return (
+          <div key={idx} className="notes">
+            <div className="note">
+              <h1>{note.tittle}</h1>
+              <p>{note.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
